aef31d65382ec596ab73066012492b6a8de1b7e7
nonny123 <justcoolk@yahoo.com>
2021-09-17 11:39:52 +0100

    Create Image Picker Wrapper

---
+++ app/src/main/java/com/androidui/imagepicker/ImageSource.kt
@@ -0,0 +1,106 @@
+package com.androidui.imagepicker
+
+import android.app.Activity
+import android.os.Environment
+import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.github.dhaval2404.imagepicker.ImagePicker
+import java.io.File
+
+class ImageSource(private val activity: AppCompatActivity, view: ImageView){
+
+    fun pickGalleryImage() {
+        ImagePicker.with(activity)
+            .galleryOnly()
+            .crop(200f, 200f)  // crop image
+            .compress(1024)  //Final image size will be less than 1 MB
+            .maxResultSize(200, 200) // Image resolution will be less than 512 x 512
+            .createIntent { intent ->
+                startForGalleryImageResult.launch(intent)
+            }
+    }
+
+    //use ActivityResult API
+    private val startForGalleryImageResult =
+        activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    //get image Uri
+                    val fileUri = data?.data!!
+                    //set imageView Uri
+                    view.setImageURI(fileUri)
+                }
+                ImagePicker.RESULT_ERROR -> {
+                    //for analytics
+                    //Log.d("ImagePicker", ImagePicker.getError(data))
+                }
+                else -> {
+                    //Log.d("ImagePicker", "Task Cancelled")
+                }
+            }
+        }
+
+
+    fun pickCameraImage() {
+        ImagePicker.with(activity)
+            // User can only capture image from Camera
+            .cameraOnly()
+            .crop(200f, 200f)
+            .compress(1024)	//Final image size will be less than 1 MB
+            //  Path: /storage/sdcard0/Android/data/package/files
+            .saveDir(activity.getExternalFilesDir(null)!!)
+            //  Path: /storage/sdcard0/Android/data/package/files/DCIM
+            .saveDir(activity.getExternalFilesDir(Environment.DIRECTORY_DCIM)!!)
+            //  Path: /storage/sdcard0/Android/data/package/files/Download
+            .saveDir(activity.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)!!)
+            //  Path: /storage/sdcard0/Android/data/package/files/Pictures
+            .saveDir(activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!)
+            //  Path: /storage/sdcard0/Android/data/package/files/Pictures/ImagePicker
+            .saveDir(File(activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!, "ImagePicker"))
+            //  Path: /storage/sdcard0/Android/data/package/files/ImagePicker
+            .saveDir(activity.getExternalFilesDir("ImagePicker")!!)
+            //  Path: /storage/sdcard0/Android/data/package/cache/ImagePicker
+            .saveDir(File(activity.externalCacheDir, "ImagePicker"))
+            //  Path: /data/data/package/cache/ImagePicker
+            .saveDir(File(activity.cacheDir, "ImagePicker"))
+            //  Path: /data/data/package/files/ImagePicker
+            .saveDir(File(activity.filesDir, "ImagePicker"))
+            .maxResultSize(200, 200) // Image resolution will be less than 512 x 512
+            .createIntent { intent ->
+                startForCameraImageResult.launch(intent)
+            }
+    }
+
+
+    //use ActivityResult API
+    private val startForCameraImageResult =
+        activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    //get image Uri
+                    val fileUri = data?.data!!
+                    //set imageView Uri
+                    view.setImageURI(fileUri)
+                }
+                ImagePicker.RESULT_ERROR -> {
+                    //for analytics
+                    //Log.d("ImagePicker", ImagePicker.getError(data))
+                }
+                else -> {
+                    //Log.d("ImagePicker", "Task Cancelled")
+                }
+            }
+        }
+}
+
+
+
+
--- app/src/main/java/com/androidui/imagepicker/MainActivity.kt
+++ app/src/main/java/com/androidui/imagepicker/MainActivity.kt
@@ -17,9 +17,10 @@ import kotlinx.android.synthetic.main.bottom_sheet_modal.view.*
 
 class MainActivity : AppCompatActivity() {
 
-
     private lateinit var imageView: ImageView
     private lateinit var btnImg: Button
+
+    private lateinit var imageSource : ImageSource
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         //set views/
         imageView = findViewById(R.id.mImgView)
         btnImg = findViewById(R.id.btnImg)
+
+        //initialize Image Picker Source
+        imageSource = ImageSource(this, imageView)
 
         imageView.setOnClickListener {
             showBottomSheetDialog()
@@ -47,139 +51,27 @@ class MainActivity : AppCompatActivity() {
         val dialog = BottomSheetDialog(this)
         dialog.setContentView(modalView)
 
-        //get linearlayout views
+        //get linearLayout views
         modalView.layoutGallery.setOnClickListener {
-            pickGalleryImage()
+            imageSource.pickGalleryImage()
             dialog.cancel()
         }
         modalView.layoutCamera.setOnClickListener {
-            pickCameraImage()
+            imageSource.pickCameraImage()
             dialog.cancel()
         }
 
-        //get textviews
+        //get textViews
         modalView.gallery_text.setOnClickListener {
-            pickGalleryImage()
+            imageSource.pickGalleryImage()
             dialog.cancel()
         }
         modalView.camera_text.setOnClickListener {
-            pickCameraImage()
+            imageSource.pickCameraImage()
             dialog.cancel()
         }
 
         dialog.show()
     }
-
-    private fun pickGalleryImage() {
-        ImagePicker.with(this)
-                .galleryOnly()
-                .crop(200f, 200f)  // crop image
-                .compress(1024)	//Final image size will be less than 1 MB
-                .setImageProviderInterceptor { imageProvider -> // Intercept ImageProvider
-                    //for analytics
-                    //Log.d("ImagePicker", "Selected ImageProvider: " + imageProvider.name)
-                }
-                .setDismissListener {
-                    //Log.d("ImagePicker", "Dialog Dismiss")
-                }
-                // Image resolution will be less than 512 x 512
-                .maxResultSize(200, 200)
-                .createIntent { intent ->
-                    startForGalleryImageResult.launch(intent)
-                }
-    }
-
-    //use ActivityResult API
-    private val startForGalleryImageResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-                val resultCode = result.resultCode
-                val data = result.data
-
-                imageView = findViewById(R.id.mImgView)
-
-                when (resultCode) {
-                    Activity.RESULT_OK -> {
-                        //Image Uri will not be null for RESULT_OK
-                        val fileUri = data?.data!!
-
-                        imageView.setImageURI(fileUri)
-                    }
-                    ImagePicker.RESULT_ERROR -> {
-                        //for analytics
-                        //Log.d("ImagePicker", ImagePicker.getError(data))
-                    }
-                    else -> {
-                        //Log.d("ImagePicker", "Task Cancelled")
-                    }
-                }
-            }
-
-
-    //use ActivityResult API
-    private fun pickCameraImage() {
-        ImagePicker.with(this)
-                // User can only capture image from Camera
-                .cameraOnly()
-                .crop(200f, 200f)
-                .compress(1024)	//Final image size will be less than 1 MB
-                //  Path: /storage/sdcard0/Android/data/package/files
-                .saveDir(getExternalFilesDir(null)!!)
-                //  Path: /storage/sdcard0/Android/data/package/files/DCIM
-                .saveDir(getExternalFilesDir(Environment.DIRECTORY_DCIM)!!)
-                //  Path: /storage/sdcard0/Android/data/package/files/Download
-                .saveDir(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)!!)
-                //  Path: /storage/sdcard0/Android/data/package/files/Pictures
-                .saveDir(getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!)
-                //  Path: /storage/sdcard0/Android/data/package/files/Pictures/ImagePicker
-                .saveDir(File(getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!, "ImagePicker"))
-                //  Path: /storage/sdcard0/Android/data/package/files/ImagePicker
-                .saveDir(getExternalFilesDir("ImagePicker")!!)
-                //  Path: /storage/sdcard0/Android/data/package/cache/ImagePicker
-                .saveDir(File(getExternalCacheDir(), "ImagePicker"))
-                //  Path: /data/data/package/cache/ImagePicker
-                .saveDir(File(getCacheDir(), "ImagePicker"))
-                //  Path: /data/data/package/files/ImagePicker
-                .saveDir(File(getFilesDir(), "ImagePicker"))
-
-                .setImageProviderInterceptor { imageProvider -> // Intercept ImageProvider
-                    //for analytics
-                    //Log.d("ImagePicker", "Selected ImageProvider: " + imageProvider.name)
-                }
-                .setDismissListener {
-                    //Log.d("ImagePicker", "Dialog Dismiss")
-                }
-                // Image resolution will be less than 512 x 512
-                .maxResultSize(200, 200)
-                .createIntent { intent ->
-                    startForCameraImageResult.launch(intent)
-                }
-    }
-
-
-    private val startForCameraImageResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-                val resultCode = result.resultCode
-                val data = result.data
-
-                imageView = findViewById(R.id.mImgView)
-
-                when (resultCode) {
-                    Activity.RESULT_OK -> {
-                        //Image Uri will not be null for RESULT_OK
-                        val fileUri = data?.data!!
-
-                        imageView.setImageURI(fileUri)
-                    }
-                    ImagePicker.RESULT_ERROR -> {
-                        //for analytics
-                        //Log.d("ImagePicker", ImagePicker.getError(data))
-                    }
-                    else -> {
-                        //Log.d("ImagePicker", "Task Cancelled")
-                    }
-                }
-            }
-
-
 
 }
